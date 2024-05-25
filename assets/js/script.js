ff2eeaafdfd587dbc5f8caee4bdd96f5b5c72198
BONAFIDE BOSS <contact@almir.info>
2024-05-25 17:56:42 +0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ Vue.createApp({
   data() {
     return {
       sitename: "AfterSchool",
-      serverURL: "https://afterschoolbackend.vercel.app/",
+      serverURL: "http://10.63.3.106:8000/",
       pages: ["Home", "My Orders"],
       currentPage: "Home",
       lessons: null,
@@ -356,20 +356,20 @@ function debounce(func, delay = 300) {
 }
 
 function notify(message, color = "success", delay = 2000, stacking = false) {
-  document.querySelectorAll(".alert").forEach((i) => i.remove());
+  document.querySelectorAll(".notification").forEach((i) => i.remove());
 
-  const alert = document.createElement("div");
-  alert.innerHTML = `
+  const notification = document.createElement("div");
+  notification.innerHTML = `
     <div class="d-flex justify-content-between gap-4">
       <p class="mb-0">${message}</p>
       <button type="button" class="btn-close" data-mdb-dismiss="alert"></button>
     </div>
   `;
-  alert.classList.add("alert", "fade", "shadow-5");
+  notification.classList.add("alert", "fade", "shadow-5",'notification');
 
-  document.body.appendChild(alert);
+  document.body.appendChild(notification);
 
-  const alertInstance = new mdb.Alert(alert, {
+  const notificationInstance = new mdb.Alert(notification, {
     color: color,
     stacking: stacking,
     offset: 20,
@@ -380,5 +380,5 @@ function notify(message, color = "success", delay = 2000, stacking = false) {
     animation: true,
   });
 
-  alertInstance.show();
+  notificationInstance.show();
 }
